3774c1bd20a0699c4daae469b0635f800028c446
Anuoluwapo Pelumi Hammed <stationphast@gmail.com>
2025-03-09 02:25:22 +0100

    basic setup

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,23 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { log } from 'console';
 
 @WebSocketGateway()
 export class ChatGateway {
+  private logger = new Logger(ChatGateway.name);
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
+  handleMessage(
+    @ConnectedSocket() client: any,
+    @MessageBody() payload: any,
+  ): string {
+    const clientID = client.id;
+    log(payload);
+    this.logger.log(`message received from: ${clientID}`);
     return 'Hello world!';
   }
 }
